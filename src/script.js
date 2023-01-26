56fad1f74e872344dbc74eed383899c1cc91aeb0
VanshArora08 <kumar.vansharora@gmail.com>
2023-01-27 01:04:52 +0530

    updated lights

--- src/script.js
+++ src/script.js
@@ -189,12 +189,12 @@ gui.add(houseLight.position, 'z')
 
 
 // Ambient light
-const ambientLight = new three.AmbientLight('#b9d5ff', 0.09)
+const ambientLight = new three.AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new three.DirectionalLight('#b9d5ff', 0.09)
+const moonLight = new three.DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
